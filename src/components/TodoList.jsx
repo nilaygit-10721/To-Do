d1044c9b218d91baaaa8afa480b5057ff96f972e
NilayPatel10721 <122658744+NilayPatel10721@users.noreply.github.com>
2024-06-30 13:27:43 +0530

    Initial commit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,5 @@
 import { useSelector } from "react-redux";
 import TodoItem from "./Todoitem";
-
 const TodoList = () => {
   const filteredTodos = useSelector((state) => {
     const todos = state.todos;
@@ -18,11 +17,22 @@ const TodoList = () => {
       return matchFilter && matchSearch;
     });
   });
-  console.log("filtered todos: ", filteredTodos);
+
+  // Sort todos so that completed todos appear last
+  const reorderedTodos = filteredTodos.slice().sort((a, b) => {
+    if (a.completed && !b.completed) {
+      return 1; // Move a to end of the array
+    } else if (!a.completed && b.completed) {
+      return -1; // Move b to end of the array
+    } else {
+      return 0; // Maintain order
+    }
+  });
+
   return (
     <ul>
       <li className="my-2 text-sm italic">All your Notes Here...</li>
-      {filteredTodos.map((todo, index) => (
+      {reorderedTodos.map((todo, index) => (
         <TodoItem key={index} todo={todo} index={index} />
       ))}
     </ul>
